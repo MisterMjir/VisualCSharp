b6650a3a031cadbe022e4a598372fa171365c2dd
Mister Mjir <39204944+MisterMjir@users.noreply.github.com>
2020-02-07 22:26:07 -0600

    Success Predictor

---
+++ Success Predictor/Success Predicter/SuccessPredicter.Designer.cs
@@ -0,0 +1,421 @@
+﻿namespace Success_Predicter
+{
+    partial class SuccessPredicter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SuccessPredicter));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Male = new System.Windows.Forms.RadioButton();
+            this.Female = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Reading = new System.Windows.Forms.NumericUpDown();
+            this.Writing = new System.Windows.Forms.NumericUpDown();
+            this.Math = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Science = new System.Windows.Forms.NumericUpDown();
+            this.Memory = new System.Windows.Forms.NumericUpDown();
+            this.Logic = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Courses = new System.Windows.Forms.ListBox();
+            this.Predict = new System.Windows.Forms.Button();
+            this.Info = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Reading)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Writing)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Math)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Science)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Memory)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Logic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(38, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gender";
+            // 
+            // Male
+            // 
+            this.Male.AutoSize = true;
+            this.Male.BackColor = System.Drawing.Color.Transparent;
+            this.Male.ForeColor = System.Drawing.Color.Snow;
+            this.Male.Location = new System.Drawing.Point(43, 70);
+            this.Male.Name = "Male";
+            this.Male.Size = new System.Drawing.Size(48, 17);
+            this.Male.TabIndex = 2;
+            this.Male.TabStop = true;
+            this.Male.Text = "Male";
+            this.Male.UseVisualStyleBackColor = false;
+            // 
+            // Female
+            // 
+            this.Female.AutoSize = true;
+            this.Female.BackColor = System.Drawing.Color.Transparent;
+            this.Female.ForeColor = System.Drawing.Color.Snow;
+            this.Female.Location = new System.Drawing.Point(43, 93);
+            this.Female.Name = "Female";
+            this.Female.Size = new System.Drawing.Size(59, 17);
+            this.Female.TabIndex = 3;
+            this.Female.Text = "Female";
+            this.Female.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label2.ForeColor = System.Drawing.Color.Snow;
+            this.label2.Location = new System.Drawing.Point(192, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 26);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Skills";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.Snow;
+            this.label3.Location = new System.Drawing.Point(194, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Reading";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.Snow;
+            this.label4.Location = new System.Drawing.Point(194, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Writing";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.ForeColor = System.Drawing.Color.Snow;
+            this.label5.Location = new System.Drawing.Point(194, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Math";
+            // 
+            // Reading
+            // 
+            this.Reading.Location = new System.Drawing.Point(248, 74);
+            this.Reading.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.Reading.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Reading.Name = "Reading";
+            this.Reading.Size = new System.Drawing.Size(120, 20);
+            this.Reading.TabIndex = 4;
+            this.Reading.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Writing
+            // 
+            this.Writing.Location = new System.Drawing.Point(248, 101);
+            this.Writing.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.Writing.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Writing.Name = "Writing";
+            this.Writing.Size = new System.Drawing.Size(120, 20);
+            this.Writing.TabIndex = 6;
+            this.Writing.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Math
+            // 
+            this.Math.Location = new System.Drawing.Point(248, 128);
+            this.Math.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.Math.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Math.Name = "Math";
+            this.Math.Size = new System.Drawing.Size(120, 20);
+            this.Math.TabIndex = 8;
+            this.Math.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.ForeColor = System.Drawing.Color.Snow;
+            this.label6.Location = new System.Drawing.Point(405, 72);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Science";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.ForeColor = System.Drawing.Color.Snow;
+            this.label7.Location = new System.Drawing.Point(405, 97);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Memory";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.ForeColor = System.Drawing.Color.Snow;
+            this.label8.Location = new System.Drawing.Point(405, 123);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Logic";
+            // 
+            // Science
+            // 
+            this.Science.Location = new System.Drawing.Point(458, 73);
+            this.Science.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.Science.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Science.Name = "Science";
+            this.Science.Size = new System.Drawing.Size(120, 20);
+            this.Science.TabIndex = 5;
+            this.Science.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Memory
+            // 
+            this.Memory.Location = new System.Drawing.Point(458, 100);
+            this.Memory.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.Memory.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Memory.Name = "Memory";
+            this.Memory.Size = new System.Drawing.Size(120, 20);
+            this.Memory.TabIndex = 7;
+            this.Memory.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Logic
+            // 
+            this.Logic.Location = new System.Drawing.Point(458, 127);
+            this.Logic.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.Logic.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Logic.Name = "Logic";
+            this.Logic.Size = new System.Drawing.Size(120, 20);
+            this.Logic.TabIndex = 9;
+            this.Logic.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label9.ForeColor = System.Drawing.Color.Snow;
+            this.label9.Location = new System.Drawing.Point(43, 178);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(93, 26);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Courses";
+            // 
+            // Courses
+            // 
+            this.Courses.FormattingEnabled = true;
+            this.Courses.Items.AddRange(new object[] {
+            "AP World History",
+            "AP Physics",
+            "AP Calculus",
+            "AP English III",
+            "AP Computer Science A",
+            "AP Music Theory",
+            "AP Political Science"});
+            this.Courses.Location = new System.Drawing.Point(48, 217);
+            this.Courses.Name = "Courses";
+            this.Courses.Size = new System.Drawing.Size(208, 173);
+            this.Courses.TabIndex = 10;
+            // 
+            // Predict
+            // 
+            this.Predict.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Predict.Location = new System.Drawing.Point(408, 277);
+            this.Predict.Name = "Predict";
+            this.Predict.Size = new System.Drawing.Size(170, 62);
+            this.Predict.TabIndex = 11;
+            this.Predict.Text = "Predict!";
+            this.Predict.UseVisualStyleBackColor = true;
+            this.Predict.Click += new System.EventHandler(this.Predict_Click);
+            // 
+            // Info
+            // 
+            this.Info.Location = new System.Drawing.Point(719, 12);
+            this.Info.Name = "Info";
+            this.Info.Size = new System.Drawing.Size(69, 40);
+            this.Info.TabIndex = 1;
+            this.Info.Text = "Help";
+            this.Info.UseVisualStyleBackColor = true;
+            this.Info.Click += new System.EventHandler(this.Info_Click);
+            // 
+            // SuccessPredicter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Info);
+            this.Controls.Add(this.Predict);
+            this.Controls.Add(this.Courses);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.Logic);
+            this.Controls.Add(this.Memory);
+            this.Controls.Add(this.Science);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Math);
+            this.Controls.Add(this.Writing);
+            this.Controls.Add(this.Reading);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Female);
+            this.Controls.Add(this.Male);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SuccessPredicter";
+            this.Text = "Success Predicter";
+            this.Load += new System.EventHandler(this.SuccessPredicter_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Reading)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Writing)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Math)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Science)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Memory)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Logic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton Male;
+        private System.Windows.Forms.RadioButton Female;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown Reading;
+        private System.Windows.Forms.NumericUpDown Writing;
+        private System.Windows.Forms.NumericUpDown Math;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown Science;
+        private System.Windows.Forms.NumericUpDown Memory;
+        private System.Windows.Forms.NumericUpDown Logic;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ListBox Courses;
+        private System.Windows.Forms.Button Predict;
+        private System.Windows.Forms.Button Info;
+    }
+}
+
---
+++ Success Predictor/Success Predicter/SuccessPredicter.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Success_Predicter
+{
+    public partial class SuccessPredicter : Form
+    {
+        bool firstTime;
+        public SuccessPredicter()
+        {
+            InitializeComponent();
+        }
+
+        private void Predict_Click(object sender, EventArgs e)
+        {
+            /*
+             * Using returns instead of if else so in case I need a new
+             * check in the future, I won't have to the copying and pasting
+             */
+             
+            // Check if a gender is selected
+            if (Male.Checked == false && Female.Checked == false)
+            {
+                MessageBox.Show("Please select a gender");
+                return;
+            }
+
+            // Check if NumericUpDowns were selected, but only the first time
+            if (firstTime)
+            {
+                if (Reading.Value == 1 && Science.Value == 1 && Writing.Value == 1 && Memory.Value == 1 && Math.Value == 1 && Logic.Value == 1)
+                {
+                    MessageBox.Show("Hey, all of your skills are set to one. If you believe that you are better than that, please select skill values that reflect your strength in those skills.");
+                    firstTime = false;
+                    return;
+                }
+            }
+            
+            // Call the function with the "mathematically-derived" (arbitrary) parameters for the course
+            string courseName = Courses.Text;
+            if (courseName.Equals("AP World History"))
+            {
+                CreatePrediction(3, 0, 1, 3, 0, 2, true);
+            }
+            else if (courseName.Equals("AP Physics"))
+            {
+                CreatePrediction(1, 4, 1, 1, 3, 2, true);
+            }
+            else if (courseName.Equals("AP Calculus"))
+            {
+                CreatePrediction(0, 0, 0, 1, 5, 1, true);
+            }
+            else if (courseName.Equals("AP English III"))
+            {
+                CreatePrediction(4, 0, 5, 1, 0, 1, false); // Haha, prediction not found
+            }
+            else if (courseName.Equals("AP Computer Science A"))
+            {
+                CreatePrediction(10, 10, 10, 10, 10, 10, true);
+            }
+            else if (courseName.Equals("AP Music Theory"))
+            {
+                CreatePrediction(0, 2, 1, 0, 1, 3, false);
+            }
+            else if (courseName.Equals("AP Political Science"))
+            {
+                CreatePrediction(3, 0, 0, 3, 0, 3, false);
+            }
+            else // If no course is selected tell the user to select a course
+            {
+                MessageBox.Show("Please select a course");
+            }
+        }
+
+        private void CreatePrediction(int minReading, int minScience, int minWriting, int minMemory, int minMath, int minLogic, bool male)
+        {
+            // Initialize string output
+            string output = "";
+            int totalSkill = 0; // Figure out overall how well you will do in a class
+
+            output += "Prediction for " + Courses.Text + "\n\nFor this class, you are,\n";
+
+            // Add the string prediction and numeric predictions for every skill
+            output += getStrSkillPrediction("Reading", minReading, (int) Reading.Value) + "\n";
+            totalSkill += getIntSkillPrediction(minReading, (int)Reading.Value);
+
+            output += getStrSkillPrediction("Science", minScience, (int) Science.Value) + "\n";
+            totalSkill += getIntSkillPrediction(minScience, (int)Science.Value);
+
+            output += getStrSkillPrediction("Writing", minWriting, (int) Writing.Value) + "\n";
+            totalSkill += getIntSkillPrediction(minWriting, (int)Writing.Value);
+
+            output += getStrSkillPrediction("Memory", minMemory, (int) Memory.Value) + "\n";
+            totalSkill += getIntSkillPrediction(minMemory, (int)Memory.Value);
+
+            output += getStrSkillPrediction("Math", minMath, (int) Math.Value) + "\n";
+            totalSkill += getIntSkillPrediction(minMath, (int)Math.Value);
+
+            output += getStrSkillPrediction("Logic", minLogic, (int) Logic.Value) + "\n";
+            totalSkill += getIntSkillPrediction(minLogic, (int)Logic.Value);
+
+            output += "\nWith a total skill score of: " + totalSkill.ToString() + ",\n" + getTotalSkillPrediction(totalSkill);
+
+            // Figure out if majority of class is not the selected gender, send message only if that's true
+            if (Male.Checked != male)
+            {
+                if (Male.Checked)
+                {
+                    output += "\n\nAlso note that most of your class will not be boys";
+                }
+                else
+                {
+                    output += "\n\nAlso note that most of your class will not be girls.";
+                }
+            }
+
+            MessageBox.Show(output);
+        }
+
+        private string getStrSkillPrediction(string skillName, int minSkillVal, int skillVal)
+        {
+            if (skillVal < minSkillVal) // User skill is less than minimum Skill
+            {
+                return "You need to be better at: " + skillName;
+            }
+            else if (skillVal > minSkillVal) // User skill is greater than minimum Skill
+            {
+                return "You are exceptional at: " + skillName;
+            }
+            else // User skill is equal to minimum Skill
+            {
+                return "You are good at: " + skillName;
+            }
+        }
+
+        private int getIntSkillPrediction(int minSkillVal, int skillVal)
+        {
+            if (skillVal < minSkillVal) // User skill is less than minimum Skill
+            {
+                return -1;
+            }
+            else if (skillVal > minSkillVal) // User skill is greater than minimum Skill
+            {
+                return 2;
+            }
+            else // User skill is equal to minimum Skill
+            {
+                return 1;
+            }
+        }
+
+        private string getTotalSkillPrediction(int totalSkill)
+        {
+            // Ratings: -6 - -1, 0 - 5, 6 - 11, and 12 - 18
+            if (totalSkill >= 12)
+            {
+                return "You will do really well in this class";
+            }
+            else if (totalSkill >= 6)
+            {
+                return "You will do ok in this class";
+            }
+            else if (totalSkill >= 0)
+            {
+                return "You will do pretty bad in this class";
+            }
+            else
+            {
+                return "You will barely survive in this class";
+            }
+        }
+
+        private void Info_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Steps\n" +
+                "1. Select your gender\n" +
+                "2. Select the value you think you have in each skill. 1 is the worst and 5 is the best\n" +
+                "3. Select a course you want to take\n" +
+                "4. Click the predict button\n\n" +
+                "Your output will tell you what skills you need to improve and how good you will do in the class, " +
+                "your total skill value is on a scale from -6 to 12 and determines how good you will do in a class" +
+                "\n\nFor further questions please email help@companysupport.com"
+                );
+        }
+
+        private void SuccessPredicter_Load(object sender, EventArgs e)
+        {
+            // Initialize variables
+            firstTime = true;
+        }
+    }
+}
