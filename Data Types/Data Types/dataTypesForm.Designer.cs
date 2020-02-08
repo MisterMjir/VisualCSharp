ae3a1616b9c1c2eac2ef1e4b6604d1d7fd9a9965
Mister Mjir <39204944+MisterMjir@users.noreply.github.com>
2020-02-07 22:16:35 -0600

    Upload Data Types

---
+++ Data Types/Data Types/dataTypesForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Data_Types
+{
+    partial class dataTypesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericButton = new System.Windows.Forms.Button();
+            this.boolButton = new System.Windows.Forms.Button();
+            this.constantButton = new System.Windows.Forms.Button();
+            this.stringButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // numericButton
+            // 
+            this.numericButton.Location = new System.Drawing.Point(353, 26);
+            this.numericButton.Name = "numericButton";
+            this.numericButton.Size = new System.Drawing.Size(120, 61);
+            this.numericButton.TabIndex = 0;
+            this.numericButton.Text = "Numeric Data";
+            this.numericButton.UseVisualStyleBackColor = true;
+            this.numericButton.Click += new System.EventHandler(this.NumericButton_Click);
+            // 
+            // boolButton
+            // 
+            this.boolButton.Location = new System.Drawing.Point(353, 134);
+            this.boolButton.Name = "boolButton";
+            this.boolButton.Size = new System.Drawing.Size(120, 61);
+            this.boolButton.TabIndex = 1;
+            this.boolButton.Text = "Boolean Data";
+            this.boolButton.UseVisualStyleBackColor = true;
+            this.boolButton.Click += new System.EventHandler(this.BoolButton_Click);
+            // 
+            // constantButton
+            // 
+            this.constantButton.Location = new System.Drawing.Point(353, 243);
+            this.constantButton.Name = "constantButton";
+            this.constantButton.Size = new System.Drawing.Size(120, 61);
+            this.constantButton.TabIndex = 2;
+            this.constantButton.Text = "Constant Data";
+            this.constantButton.UseVisualStyleBackColor = true;
+            this.constantButton.Click += new System.EventHandler(this.ConstantButton_Click);
+            // 
+            // stringButton
+            // 
+            this.stringButton.Location = new System.Drawing.Point(353, 350);
+            this.stringButton.Name = "stringButton";
+            this.stringButton.Size = new System.Drawing.Size(120, 61);
+            this.stringButton.TabIndex = 3;
+            this.stringButton.Text = "String Data";
+            this.stringButton.UseVisualStyleBackColor = true;
+            this.stringButton.Click += new System.EventHandler(this.StringButton_Click);
+            // 
+            // dataTypesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.stringButton);
+            this.Controls.Add(this.constantButton);
+            this.Controls.Add(this.boolButton);
+            this.Controls.Add(this.numericButton);
+            this.Name = "dataTypesForm";
+            this.Text = "Mad Lab Experiment with Data Types (Wear Safety Goggles)";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button numericButton;
+        private System.Windows.Forms.Button boolButton;
+        private System.Windows.Forms.Button constantButton;
+        private System.Windows.Forms.Button stringButton;
+    }
+}
+
---
+++ Data Types/Data Types/dataTypesForm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Data_Types
+{
+    public partial class dataTypesForm : Form
+    {
+        public dataTypesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void NumericButton_Click(object sender, EventArgs e)
+        {
+            // Initialize numeric variables
+            int myInt = 54;
+            double myDouble = 21.624;
+            decimal myDecimal = 7.2934780235M;
+            float myFloat = 45.3F;
+
+            // Display the numbers
+            MessageBox.Show("The integer is: " + myInt.ToString());
+            MessageBox.Show("The double is: " + myDouble.ToString());
+            MessageBox.Show("The decimal is: " + myDecimal.ToString());
+            MessageBox.Show("The float is: " + myFloat.ToString());
+        }
+
+        private void BoolButton_Click(object sender, EventArgs e)
+        {
+            // Initialize boolean variables
+            bool myBool = true;
+
+            // Display the boolean
+            MessageBox.Show("The boolean is: " + myBool.ToString());
+        }
+
+        private void ConstantButton_Click(object sender, EventArgs e)
+        {
+            // Initialize the constants
+            const double pi = 3.14159;
+            const int myInt = 62;
+
+            // Display the constants
+            MessageBox.Show("The constant double is: " + pi.ToString());
+            MessageBox.Show("The constant int is: " + myInt.ToString());
+        }
+
+        private void StringButton_Click(object sender, EventArgs e)
+        {
+            // Initialize text variables
+            char myChar = '^';
+            string myString = "_^ Wait, where's the left side of my face?";
+
+            // Display text values
+            MessageBox.Show("The character is: " + myChar);
+            MessageBox.Show("The string is: " + myString);
+        }
+    }
+}
