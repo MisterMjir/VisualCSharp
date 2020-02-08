42f9c97a7a636257713b86c3a94fb268bc490d38
Mister Mjir <39204944+MisterMjir@users.noreply.github.com>
2020-02-07 22:08:31 -0600

    Upload Beepers

---
+++ Beepers/Beepers/Beepers.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Beepers
+{
+    partial class Beepers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.forButton = new System.Windows.Forms.Button();
+            this.whileButton = new System.Windows.Forms.Button();
+            this.doButton = new System.Windows.Forms.Button();
+            this.numBeepsText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // forButton
+            // 
+            this.forButton.Location = new System.Drawing.Point(341, 151);
+            this.forButton.Name = "forButton";
+            this.forButton.Size = new System.Drawing.Size(152, 63);
+            this.forButton.TabIndex = 0;
+            this.forButton.Text = "For Loop Beep";
+            this.forButton.UseVisualStyleBackColor = true;
+            this.forButton.Click += new System.EventHandler(this.ForButton_Click);
+            // 
+            // whileButton
+            // 
+            this.whileButton.Location = new System.Drawing.Point(341, 255);
+            this.whileButton.Name = "whileButton";
+            this.whileButton.Size = new System.Drawing.Size(152, 63);
+            this.whileButton.TabIndex = 1;
+            this.whileButton.Text = "While Loop Beep";
+            this.whileButton.UseVisualStyleBackColor = true;
+            this.whileButton.Click += new System.EventHandler(this.WhileButton_Click);
+            // 
+            // doButton
+            // 
+            this.doButton.Location = new System.Drawing.Point(341, 363);
+            this.doButton.Name = "doButton";
+            this.doButton.Size = new System.Drawing.Size(152, 63);
+            this.doButton.TabIndex = 2;
+            this.doButton.Text = "Do Loop Beep";
+            this.doButton.UseVisualStyleBackColor = true;
+            this.doButton.Click += new System.EventHandler(this.DoButton_Click);
+            // 
+            // numBeepsText
+            // 
+            this.numBeepsText.Location = new System.Drawing.Point(436, 72);
+            this.numBeepsText.Name = "numBeepsText";
+            this.numBeepsText.Size = new System.Drawing.Size(57, 20);
+            this.numBeepsText.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(338, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Number of beeps:";
+            // 
+            // Beepers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numBeepsText);
+            this.Controls.Add(this.doButton);
+            this.Controls.Add(this.whileButton);
+            this.Controls.Add(this.forButton);
+            this.Name = "Beepers";
+            this.Text = "Beep Bloop Beep!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button forButton;
+        private System.Windows.Forms.Button whileButton;
+        private System.Windows.Forms.Button doButton;
+        private System.Windows.Forms.TextBox numBeepsText;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Beepers/Beepers/Beepers.cs
@@ -0,0 +1,136 @@
+﻿/*
+ * NOTE
+ * The Console.Beep() function doesn't work in a loop unless there is something
+ * pausing the loop, such as an alert from MessageBox.Show(). You will get the correct
+ * amount of beeps if you uncomment the MessageBox.Show() that is showing "i" (iterator),
+ * but then you will have to always confirm before hearing the next beep.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Beepers
+{
+    public partial class Beepers : Form
+    {
+        public Beepers()
+        {
+            InitializeComponent();
+        }
+
+        private void ForButton_Click(object sender, EventArgs e)
+        {
+            // Initialize Num Beeps to the number in the text box
+            try
+            {
+                int numBeeps = int.Parse(numBeepsText.Text);
+                // MessageBox.Show(numBeeps.ToString());
+
+                if (numBeeps >= 0)
+                {
+                    for (int i = 0; i < numBeeps; i++)
+                    {
+                        Console.Beep(); // Beep the computer
+                        // MessageBox.Show(i.ToString());
+                    }
+                    // Loop as many times as numBeeps
+                    /* Testing for loops
+                    int i = 0;
+                    for (; i < numBeeps; Console.Beep())
+                    {
+                        MessageBox.Show(i.ToString());
+                        {
+                            {
+                                {
+                                    i++;
+                                }
+                            }
+                        }
+                    }
+                    */
+                    /*
+                    for (int i = 0; i++ < numBeeps; ({Console.Beep(); MessageBox.Show(i.toString())})) {}
+                    */
+                }
+                else
+                {
+                    MessageBox.Show("You can't beep negative times");
+                    numBeepsText.Text = "";
+                }
+            }
+            catch // Sorry, I forgot the name of the exception
+            {
+                MessageBox.Show("Input is not formatted correctly");
+                numBeepsText.Text = "";
+            }
+        }
+
+        private void WhileButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Initialize variables
+                int i = 0;
+                int numBeeps = int.Parse(numBeepsText.Text);
+                // MessageBox.Show(numBeeps.ToString());
+
+                if (numBeeps >= 0)
+                {
+                    // Loop as many times as numBeeps
+                    while (i++ < numBeeps)
+                    {
+                        Console.Beep();
+                        // MessageBox.Show(i.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("You can't beep negative times");
+                    numBeepsText.Text = "";
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Input is not formatted correctly");
+                numBeepsText.Text = "";
+            }
+        }
+
+        private void DoButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Initialize variables
+                int i = 0;
+                int numBeeps = int.Parse(numBeepsText.Text);
+                // MessageBox.Show(numBeeps.ToString());
+
+                if (numBeeps >= 0)
+                {
+                    // Loop as many times as numBeeps
+                    do
+                    {
+                        Console.Beep();
+                         MessageBox.Show(i.ToString());
+                    } while (++i < numBeeps);
+                }
+                else
+                {
+                    MessageBox.Show("You can't beep negative times");
+                    numBeepsText.Text = "";
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Input is not formatted correctly");
+                numBeepsText.Text = "";
+            }
+        }
+    }
+}
