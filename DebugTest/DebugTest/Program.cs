7d6e052b9883903e67ef56ad98462d421fce53bf
Mister Mjir <39204944+MisterMjir@users.noreply.github.com>
2020-02-07 22:18:50 -0600

    Upload Debug Test

---
+++ DebugTest/DebugTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine(add(1, 3));
+        }
+
+        static int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
