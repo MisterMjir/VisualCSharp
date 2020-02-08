7e5ea84a41645fc0d52167a287be370d7688ae24
Mister Mjir <39204944+MisterMjir@users.noreply.github.com>
2020-02-07 22:27:44 -0600

    Upload ToDo List

---
+++ ToDo List/ToDo List/ToDoList.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDo_List
+{
+    public partial class ToDoList : Form
+    {
+        System.Collections.Generic.LinkedList<string> todoList = new LinkedList<string>();
+        public ToDoList()
+        {
+            InitializeComponent();
+        }
+
+        private void AddFrontButton_Click(object sender, EventArgs e)
+        {
+            if (ItemTextBox.Text != "")
+            {
+                todoList.AddFirst(ItemTextBox.Text);
+            }
+            DisplayList();
+        }
+
+        private void AddBackButton_Click(object sender, EventArgs e)
+        {
+            if (ItemTextBox.Text != "")
+            {
+                todoList.AddLast(ItemTextBox.Text);
+            }
+            DisplayList();
+        }
+
+        private void RemoveButton_Click(object sender, EventArgs e)
+        {
+            /*
+             * There has to be a better way than what CompuScholar has
+             * because if there are two items with the same name, and the
+             * user selects the second item, the first item will get deleted
+             * 
+             * I will still do the CompuScholar method
+             */
+             if (ToDoListBox.Text != "")
+             {
+                todoList.Remove(ToDoListBox.Text);
+                DisplayList();
+             }
+             
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            todoList.Clear();
+            DisplayList();
+        }
+
+        private void DisplayList()
+        {
+            ItemTextBox.Text = "";
+            ToDoListBox.Items.Clear();
+            foreach(string todo in todoList)
+            {
+                ToDoListBox.Items.Add(todo);
+            }
+        }
+    }
+}
