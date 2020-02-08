2c6f79a56126573b3658b5e5acf456225b419eb9
Mister Mjir <39204944+MisterMjir@users.noreply.github.com>
2020-02-07 22:26:53 -0600

    Upload Tall Tales

---
+++ Tall Tales/Tall Tales/tallTales.Designer.cs
@@ -0,0 +1,499 @@
+﻿namespace Tall_Tales
+{
+    partial class tallTales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.speciesComboBox = new System.Windows.Forms.ComboBox();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.activityList = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.goodBadList = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.finalStoryText = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.createStory = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // speciesComboBox
+            // 
+            this.speciesComboBox.FormattingEnabled = true;
+            this.speciesComboBox.Items.AddRange(new object[] {
+            "page",
+            "acoustics",
+            "chickens",
+            "actor",
+            "care",
+            "reaction",
+            "quince",
+            "boy",
+            "rice",
+            "trouble",
+            "bear",
+            "seashore",
+            "donkey",
+            "zebra",
+            "hole",
+            "system",
+            "toes",
+            "nest",
+            "nerve",
+            "cows",
+            "thread",
+            "flame",
+            "deer",
+            "plantation",
+            "calculator",
+            "request",
+            "rat",
+            "angle",
+            "place",
+            "songs",
+            "crowd",
+            "mitten",
+            "lace",
+            "minute",
+            "turkey",
+            "sink",
+            "flavor",
+            "button",
+            "lamp",
+            "view",
+            "friend",
+            "shoes",
+            "woman",
+            "part",
+            "throat",
+            "pets",
+            "hour",
+            "shade",
+            "cream",
+            "cough",
+            "mark",
+            "bee",
+            "spy",
+            "frogs",
+            "airplane",
+            "back",
+            "badge",
+            "cast",
+            "pest",
+            "eyes",
+            "account",
+            "sky",
+            "stream",
+            "sense",
+            "move",
+            "star",
+            "hate",
+            "wing",
+            "boat",
+            "title",
+            "mask",
+            "cellar",
+            "scale",
+            "reward",
+            "pigs",
+            "grain",
+            "cars",
+            "pot",
+            "loaf",
+            "laugh",
+            "lock",
+            "passenger",
+            "pancake",
+            "hot",
+            "toothpaste",
+            "disovery",
+            "pig",
+            "advice",
+            "rabbits",
+            "faries",
+            "time",
+            "dust",
+            "tub",
+            "cart",
+            "weight",
+            "lumber",
+            "trip",
+            "recess",
+            "meal",
+            "selection"});
+            this.speciesComboBox.Location = new System.Drawing.Point(163, 22);
+            this.speciesComboBox.Name = "speciesComboBox";
+            this.speciesComboBox.Size = new System.Drawing.Size(121, 21);
+            this.speciesComboBox.TabIndex = 0;
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(472, 22);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.NameTextBox.TabIndex = 1;
+            // 
+            // activityList
+            // 
+            this.activityList.FormattingEnabled = true;
+            this.activityList.Items.AddRange(new object[] {
+            "soak",
+            "stay",
+            "invite",
+            "post",
+            "waste",
+            "peep",
+            "retirm",
+            "bless",
+            "close",
+            "contain",
+            "grate",
+            "warn",
+            "whisper",
+            "report",
+            "yawn",
+            "perform",
+            "count",
+            "charge",
+            "vanish",
+            "guard",
+            "camp",
+            "rob",
+            "twist",
+            "wave",
+            "scorch",
+            "reject",
+            "chase",
+            "boast",
+            "request",
+            "curve",
+            "cough",
+            "tug",
+            "blush",
+            "pass",
+            "permit",
+            "owe",
+            "excuse",
+            "consider",
+            "fold",
+            "sin",
+            "support",
+            "appreciate",
+            "command",
+            "brake",
+            "attract",
+            "use",
+            "remind",
+            "tame",
+            "add",
+            "face",
+            "marry",
+            "rain",
+            "water",
+            "chew",
+            "divide",
+            "provide",
+            "satisfy",
+            "fade",
+            "sparkle",
+            "confuse",
+            "crack",
+            "bow",
+            "taste",
+            "x-ray",
+            "clean",
+            "copy",
+            "explode",
+            "harass",
+            "decide",
+            "time",
+            "heal",
+            "color",
+            "trick",
+            "spark",
+            "repair",
+            "welcome",
+            "offend",
+            "reign",
+            "moan",
+            "approve",
+            "pump",
+            "bury",
+            "blot",
+            "scream",
+            "burn",
+            "chop",
+            "walk",
+            "slow",
+            "guarantee",
+            "cry",
+            "sip",
+            "manage",
+            "analyse",
+            "memorize",
+            "drip",
+            "wonder",
+            "cheer",
+            "stitch",
+            "bat",
+            "protect"});
+            this.activityList.Location = new System.Drawing.Point(142, 70);
+            this.activityList.Name = "activityList";
+            this.activityList.Size = new System.Drawing.Size(120, 95);
+            this.activityList.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(313, 70);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(66, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Dabbing";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(313, 93);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(66, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Rapping";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(313, 116);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(67, 17);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "Clapping";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(16, 223);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(68, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Avocado";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(16, 246);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(58, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Stonks";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(15, 269);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(58, 17);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Kronks";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // goodBadList
+            // 
+            this.goodBadList.FormattingEnabled = true;
+            this.goodBadList.Items.AddRange(new object[] {
+            "good",
+            "bad"});
+            this.goodBadList.Location = new System.Drawing.Point(553, 196);
+            this.goodBadList.Name = "goodBadList";
+            this.goodBadList.Size = new System.Drawing.Size(75, 43);
+            this.goodBadList.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "In the beginning, there was a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(291, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "who was known to his comrades as";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "This dood was always";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(268, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "and";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 196);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(405, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "One eventful day, a day that would shake the world, this epic thing encountered a" +
+    "(n)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(489, 196);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "This was a";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(634, 196);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "dawn";
+            // 
+            // finalStoryText
+            // 
+            this.finalStoryText.Location = new System.Drawing.Point(16, 348);
+            this.finalStoryText.Multiline = true;
+            this.finalStoryText.Name = "finalStoryText";
+            this.finalStoryText.Size = new System.Drawing.Size(726, 90);
+            this.finalStoryText.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(16, 329);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(57, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Your story:";
+            // 
+            // createStory
+            // 
+            this.createStory.Location = new System.Drawing.Point(370, 295);
+            this.createStory.Name = "createStory";
+            this.createStory.Size = new System.Drawing.Size(75, 23);
+            this.createStory.TabIndex = 19;
+            this.createStory.Text = "Makestory";
+            this.createStory.UseVisualStyleBackColor = true;
+            this.createStory.Click += new System.EventHandler(this.CreateStory_Click);
+            // 
+            // tallTales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.createStory);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.finalStoryText);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.goodBadList);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.activityList);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.speciesComboBox);
+            this.Name = "tallTales";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox speciesComboBox;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.ListBox activityList;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.ListBox goodBadList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox finalStoryText;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button createStory;
+    }
+}
+
---
+++ Tall Tales/Tall Tales/tallTales.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * This is a lot like the first coding project I made in Scratch
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tall_Tales
+{
+    public partial class tallTales : Form
+    {
+        public tallTales()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateStory_Click(object sender, EventArgs e)
+        {
+            finalStoryText.Text = ""; // Reset the story
+            string myStoryText = "In the beginning, there was a(n) ";
+            myStoryText += speciesComboBox.Text;
+            myStoryText += " who was known to his comrades as ";
+            myStoryText += NameTextBox.Text;
+            myStoryText += ". This dood was always ";
+            myStoryText += activityList.Text;
+            if (checkBox1.Checked || checkBox2.Checked || checkBox3.Checked)
+            {
+                if (checkBox1.Checked)
+                {
+                    myStoryText += " and ";
+                    myStoryText += checkBox1.Text;
+                }
+                if (checkBox2.Checked)
+                {
+                    myStoryText += " and ";
+                    myStoryText += checkBox2.Text;
+                }
+                if (checkBox3.Checked)
+                {
+                    myStoryText += " and ";
+                    myStoryText += checkBox3.Text;
+                }
+                myStoryText += ".";
+            }
+            else
+            {
+                myStoryText += " and nothing.";
+            }
+            myStoryText += " One eventful day, a day that would shake the world, this epic thing encountered a(n) ";
+            if (radioButton1.Checked)
+            {
+                myStoryText += radioButton1.Text;
+            }
+            else if (radioButton2.Checked)
+            {
+                myStoryText += radioButton2.Text;
+            }
+            else if (radioButton3.Checked)
+            {
+                myStoryText += radioButton3.Text;
+            }
+            else
+            {
+                myStoryText += " hold up. You weren't supposed to get here";
+            }
+            myStoryText += ". This was a ";
+            myStoryText += goodBadList.Text;
+            myStoryText += " dawn.";
+            finalStoryText.Text = myStoryText;
+        }
+    }
+}
