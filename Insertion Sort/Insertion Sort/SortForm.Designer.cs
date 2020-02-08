08e203fdda23db581990929b032afd263b72a8e6
Mister Mjir <39204944+MisterMjir@users.noreply.github.com>
2020-02-07 22:24:19 -0600

    Upload Insertion Sort

---
+++ Insertion Sort/Insertion Sort/SortForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Insertion_Sort
+{
+    partial class SortForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddNumButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SortButton = new System.Windows.Forms.Button();
+            this.UpDownList = new System.Windows.Forms.NumericUpDown();
+            this.NumberList = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.UpDownList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AddNumButton
+            // 
+            this.AddNumButton.Location = new System.Drawing.Point(177, 86);
+            this.AddNumButton.Name = "AddNumButton";
+            this.AddNumButton.Size = new System.Drawing.Size(75, 23);
+            this.AddNumButton.TabIndex = 0;
+            this.AddNumButton.Text = "Add Number";
+            this.AddNumButton.UseVisualStyleBackColor = true;
+            this.AddNumButton.Click += new System.EventHandler(this.AddFrontButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(177, 173);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 23);
+            this.ClearButton.TabIndex = 1;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Location = new System.Drawing.Point(177, 115);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(75, 23);
+            this.RemoveButton.TabIndex = 2;
+            this.RemoveButton.Text = "Remove";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            this.RemoveButton.Click += new System.EventHandler(this.RemoveButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Please enter a new number";
+            // 
+            // SortButton
+            // 
+            this.SortButton.Location = new System.Drawing.Point(177, 144);
+            this.SortButton.Name = "SortButton";
+            this.SortButton.Size = new System.Drawing.Size(75, 23);
+            this.SortButton.TabIndex = 8;
+            this.SortButton.Text = "Sort";
+            this.SortButton.UseVisualStyleBackColor = true;
+            this.SortButton.Click += new System.EventHandler(this.SortButton_Click);
+            // 
+            // UpDownList
+            // 
+            this.UpDownList.Location = new System.Drawing.Point(38, 46);
+            this.UpDownList.Name = "UpDownList";
+            this.UpDownList.Size = new System.Drawing.Size(214, 20);
+            this.UpDownList.TabIndex = 9;
+            // 
+            // NumberList
+            // 
+            this.NumberList.FormattingEnabled = true;
+            this.NumberList.Location = new System.Drawing.Point(38, 86);
+            this.NumberList.Name = "NumberList";
+            this.NumberList.Size = new System.Drawing.Size(120, 108);
+            this.NumberList.TabIndex = 10;
+            // 
+            // SortForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 226);
+            this.Controls.Add(this.NumberList);
+            this.Controls.Add(this.UpDownList);
+            this.Controls.Add(this.SortButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RemoveButton);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.AddNumButton);
+            this.Name = "SortForm";
+            this.Text = "My Insertion Sort";
+            ((System.ComponentModel.ISupportInitialize)(this.UpDownList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddNumButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SortButton;
+        private System.Windows.Forms.NumericUpDown UpDownList;
+        private System.Windows.Forms.ListBox NumberList;
+    }
+}
+
---
+++ Insertion Sort/Insertion Sort/SortForm.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+/* TeenCoder: Windows Programming
+   
+   ToDo List Application
+
+   Copyright 2013 CompuScholar, Inc.
+*/
+
+namespace Insertion_Sort
+{
+    public partial class SortForm : Form
+    {
+        // Create and initialize the linked list of integers
+        LinkedList<int> numericList = new LinkedList<int>();
+
+        public SortForm()
+        {
+            InitializeComponent();
+        }
+
+        // This code is provided for the student in the Activity Starter
+        private void displayList()
+        {
+            // Clear the listbox 
+            NumberList.Items.Clear();
+
+            // Iterate over each item in the list
+            foreach (int item in numericList)
+            {
+                // Add the item to the text box
+                NumberList.Items.Add(item);
+            }
+        }
+
+        // This code is provided for the student in the Activity Starter
+        private void AddFrontButton_Click(object sender, EventArgs e)
+        {
+            // Get the text from the item text box
+            int newItem = (int)UpDownList.Value;
+
+            // Add to the front of the list
+            numericList.AddFirst(newItem);
+
+            // Display the new list
+            displayList();
+        }
+
+        // This code is provided for the student in the Activity Starter
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            // Remove all items from the list
+            numericList.Clear();
+
+            // Display the new list
+            displayList();
+        }
+
+        // This code is provided for the student in the Activity Starter
+        private void RemoveButton_Click(object sender, EventArgs e)
+        {
+        	  if ((NumberList.Text == null) || ( NumberList.Text.Length == 0))
+        	  	return;
+        	  	
+            // Get the text from the item text box
+            int removeItem = int.Parse(NumberList.Text);
+
+            // Iterate over the list looking for a matching string
+            LinkedListNode<int> node = numericList.First;
+            while (node != null)
+            {
+                if (node.Value == removeItem)
+                {
+                    // Remove this node from the list
+                    numericList.Remove(node);
+                    break;  // end of search
+                }
+                
+                // Move to the next entry in the list
+                node = node.Next;   
+            }
+
+            // Display the new list
+            displayList();
+        }
+
+        // This code is ADDED FOR ACTIVITY by the student
+        private void SortButton_Click(object sender, EventArgs e)
+        {
+            // There are one or less items so the list is already sorted
+            if (numericList.Count < 2)
+                return;
+
+            LinkedListNode<int> currentNode = numericList.First.Next;
+
+            while (currentNode != null)
+            {
+                LinkedListNode<int> nextNode = currentNode.Next;
+                numericList.Remove(currentNode);
+                LinkedListNode<int> searchNode = numericList.First;
+                while (searchNode != null && (int) searchNode.Value < (int) currentNode.Value)
+                {
+                    searchNode = searchNode.Next;
+                }
+
+                if (searchNode != null)
+                {
+                    numericList.AddBefore(searchNode, currentNode);
+                }
+                else
+                {
+                    numericList.AddLast(currentNode);
+                }
+
+                currentNode = nextNode;
+            }
+
+            displayList();
+        }
+    }
+}
