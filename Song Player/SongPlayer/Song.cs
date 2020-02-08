45f7c775c5db8f38c44b3b105fe3cafb09af9fae
Mister Mjir <39204944+MisterMjir@users.noreply.github.com>
2020-02-07 22:25:12 -0600

    Upload Song Player

---
+++ Song Player/SongPlayer/Song.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+/* TeenCoder: Windows Programming
+   
+   Song Player Application
+
+   Copyright 2013 CompuScholar, Inc.
+*/
+
+namespace SongPlayer
+{
+
+    // This class is provided as part of the Activity Starter program.
+    class Note
+    {
+        // Define common note frequencies
+        public static int NOTE_C = 524;
+        public static int NOTE_D = 594;
+        public static int NOTE_E = 660;
+        public static int NOTE_F = 698;
+        public static int NOTE_G = 784;
+        public static int NOTE_A = 880;
+        public static int NOTE_B = 988;
+        public static int NOTE_REST = 37;
+
+        // Define common note durations (in milliseconds)
+        public static int DURATION_WHOLE = 1600;
+        public static int DURATION_HALF = 800;
+        public static int DURATION_QUARTER = 400;
+        public static int DURATION_EIGHT = 200;
+
+        // Declare the properties for this note
+        public int Frequency;
+        public int Duration;
+
+        // This constructor requires the Frequency and Duration for this note
+        public Note(int frequency, int duration)
+        {
+            Frequency = frequency;
+            Duration = duration;
+        }
+    }
+
+    // This class code is ADDED FOR ACTIVITY by the student
+    class Song
+    {
+        public string name;
+        public LinkedList<Note> notes;
+
+        public Song(string name)
+        {
+            this.name = name;
+            notes = new LinkedList<Note>();
+        }
+        public void addNote(int freq, int duration)
+        {
+            notes.AddLast(new Note(freq, duration));
+        }
+
+        public void play()
+        {
+            foreach (Note n in notes)
+            {
+                Console.Beep(n.Frequency, n.Duration);
+            }
+        }
+
+    }
+
+}
---
+++ Song Player/SongPlayer/SongPlayerForm.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+/* TeenCoder: Windows Programming
+   
+   Song Player Application
+
+   Copyright 2013 CompuScholar, Inc.
+*/
+
+namespace SongPlayer
+{
+    public partial class SongPlayerForm : Form
+    {
+        // the form data consists of a list of songs
+        LinkedList<Song> songs;
+
+        // the constructor is provided complete as part of the activity starter
+        public SongPlayerForm()
+        {
+            InitializeComponent();
+
+            // fill out our songs array
+            initializeSongs();
+
+        }
+
+        // This code is ADDED FOR ACTIVITY by the student
+        private void initializeSongs()
+        {
+            songs = new LinkedList<Song>();
+
+            Song mario = new Song("Mario");
+            mario.addNote(Note.NOTE_E, Note.DURATION_EIGHT);
+            mario.addNote(Note.NOTE_E, Note.DURATION_EIGHT);
+            mario.addNote(Note.NOTE_REST, Note.DURATION_EIGHT);
+            mario.addNote(Note.NOTE_E, Note.DURATION_EIGHT);
+            mario.addNote(Note.NOTE_REST, Note.DURATION_EIGHT);
+            mario.addNote(Note.NOTE_C, Note.DURATION_EIGHT);
+            mario.addNote(Note.NOTE_E, Note.DURATION_EIGHT);
+            mario.addNote(Note.NOTE_REST, Note.DURATION_EIGHT);
+            mario.addNote(Note.NOTE_G, Note.DURATION_QUARTER);
+            mario.addNote(Note.NOTE_REST, Note.DURATION_QUARTER);
+            mario.addNote(Note.NOTE_G, Note.DURATION_QUARTER);
+            songs.AddLast(mario);
+            SongListBox.Items.Add(mario.name);
+
+            Song hcb = new Song("Hot Cross Buns");
+            hcb.addNote(Note.NOTE_E, Note.DURATION_QUARTER);
+            hcb.addNote(Note.NOTE_D, Note.DURATION_QUARTER);
+            hcb.addNote(Note.NOTE_C, Note.DURATION_HALF);
+            hcb.addNote(Note.NOTE_E, Note.DURATION_QUARTER);
+            hcb.addNote(Note.NOTE_D, Note.DURATION_QUARTER);
+            hcb.addNote(Note.NOTE_C, Note.DURATION_HALF);
+            hcb.addNote(Note.NOTE_E, Note.DURATION_EIGHT);
+            hcb.addNote(Note.NOTE_E, Note.DURATION_EIGHT);
+            hcb.addNote(Note.NOTE_E, Note.DURATION_EIGHT);
+            hcb.addNote(Note.NOTE_E, Note.DURATION_EIGHT);
+            hcb.addNote(Note.NOTE_D, Note.DURATION_EIGHT);
+            hcb.addNote(Note.NOTE_D, Note.DURATION_EIGHT);
+            hcb.addNote(Note.NOTE_D, Note.DURATION_EIGHT);
+            hcb.addNote(Note.NOTE_D, Note.DURATION_EIGHT);
+            hcb.addNote(Note.NOTE_E, Note.DURATION_QUARTER);
+            hcb.addNote(Note.NOTE_D, Note.DURATION_QUARTER);
+            hcb.addNote(Note.NOTE_C, Note.DURATION_WHOLE);
+            songs.AddLast(hcb);
+            SongListBox.Items.Add(hcb.name);
+
+            Song randomNotes = new Song("RANDOM NOTES!!!");
+            randomNotes.addNote(Note.NOTE_A, Note.DURATION_HALF);
+            randomNotes.addNote(Note.NOTE_B, Note.DURATION_EIGHT);
+            randomNotes.addNote(Note.NOTE_G, Note.DURATION_EIGHT);
+            randomNotes.addNote(Note.NOTE_E, Note.DURATION_WHOLE);
+            randomNotes.addNote(Note.NOTE_A, Note.DURATION_EIGHT);
+            randomNotes.addNote(Note.NOTE_B, Note.DURATION_HALF);
+            randomNotes.addNote(Note.NOTE_C, Note.DURATION_WHOLE);
+            randomNotes.addNote(Note.NOTE_A, Note.DURATION_EIGHT);
+            randomNotes.addNote(Note.NOTE_A, Note.DURATION_EIGHT);
+            randomNotes.addNote(Note.NOTE_D, Note.DURATION_QUARTER);
+            songs.AddLast(randomNotes);
+            SongListBox.Items.Add(randomNotes.name);
+        }
+
+        // This function is provided as part of the activity starter.
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            // get the current selection index
+            int selection = SongListBox.SelectedIndex;
+            if (selection >= 0)
+            {
+                // get the selected song from the list
+                Song s = songs.ElementAt(selection);
+
+                // play the song
+                s.play();
+            }
+
+        }
+    }
+}
